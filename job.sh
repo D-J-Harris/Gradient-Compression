7aef6333d1b1a12d50d300141e16861a1b891a14
Daniel Harris <09harrisda@gmail.com>
2020-06-29 16:31:18 +0100

    changed job details for gpu

--- job.sh
+++ job.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 python -u main.py \
---hidden_size 600 \
+--hidden_size 300 \
 --num_workers 1 \
 --seq_length 35 \
---num_layers 4 \
+--num_layers 2 \
 --batch_size_train 32 \
 --dropout_prob 0.5 \
 --initial_lr 20 \
 --tie_weights \
+--cuda \
+--wandb \
 --memory 'none' \
 --compression 'none' \
 --project_name 'project_name' \
